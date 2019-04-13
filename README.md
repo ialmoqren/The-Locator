42f1874dd3cf2dd7dee27d0033aa60c81c4b2577
Ibrahim Almuqrin <ialmoqren@gmail.com>
2019-04-13 17:08:19 +0300

    Improved the readme file + fixed the login bug

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # The-Locator
-So, This site is supposed to use Google Maps APIs to show an address of a place the user searched for,
-I used MAMP as a local server to run the web site.
+A website that allows users to get the address of any place they are looking for.
+
+The website uses Google Maps APIs and was made to test my skills in utilizing public APIs, creating a login page and using bootstrap
+
+
+
+## Demo
+
+<img src="./demo/demo.gif" alt="drawing" width="250"/>
+
+
+
+## Running locally
+
+### 1. Database setup
+
+You should have am up and running local mysql database with the following details
+
+	username = "root"
+	password = "root"
+	dbname   = "demo"
+
+
+You should have one simple table named `users`, structured as the following
+ 
+<img src="./readmeScreenshots/usersTable.png" alt="drawing" width="150"/>
+
+### 2. Installation & Development
+
+Clone the repository
+```shell
+git clone https://github.com/ialmoqren/The-Locator
+```
+
+Navigate to the project directory
+```shell
+cd The-Locator
+```
+
+You serve the php project the way you like, but for a quick test run
+```shell
+php -S localhost:8080
+```
+
+Then open the link [http://localhost:8080](http://localhost:8080)
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<!--// build a login system with PHP and MySQL:
-https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php-->
-
-
 <?php
+
+// build a login system with PHP and MySQL:
+// https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php-->
+
 // Include config file
 require_once 'config.php';
  
@@ -11,28 +11,28 @@ $username = $password = "";
 $username_err = $password_err = "";
  
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
  
     // Check if username is empty
-    if(empty(trim($_POST["username"]))){
+    if (empty(trim($_POST["username"]))) {
         $username_err = 'Please enter username.';
-    } else{
+    } else {
         $username = trim($_POST["username"]);
     }
     
     // Check if password is empty
-    if(empty(trim($_POST['password']))){
+    if (empty(trim($_POST['password']))) {
         $password_err = 'Please enter your password.';
-    } else{
+    } else {
         $password = trim($_POST['password']);
     }
     
     // Validate credentials
-    if(empty($username_err) && empty($password_err)){
+    if (empty($username_err) && empty($password_err)) {
         // Prepare a select statement
         $sql = "SELECT username, password FROM users WHERE username = ?";
         
-        if($stmt = mysqli_prepare($link, $sql)){
+        if ($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
             
@@ -40,31 +40,31 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $param_username = $username;
             
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
+            if (mysqli_stmt_execute($stmt)) {
                 // Store result
                 mysqli_stmt_store_result($stmt);
                 
                 // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                if (mysqli_stmt_num_rows($stmt) == 1) {
                     // Bind result variables
                     mysqli_stmt_bind_result($stmt, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
+                    if (mysqli_stmt_fetch($stmt)) {
+                        if ($password == $hashed_password) {
                             /* Password is correct, so start a new session and
                             save the username to the session */
                             session_start();
-                            $_SESSION['username'] = $username;      
+                            $_SESSION['username'] = $username;
                             header("location: welcome.php");
-                        } else{
+                        } else {
                             // Display an error message if password is not valid
                             $password_err = 'The password you entered was not valid.';
                         }
                     }
-                } else{
+                } else {
                     // Display an error message if username doesn't exist
                     $username_err = 'No account found with that username.';
                 }
-            } else{
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
         }
